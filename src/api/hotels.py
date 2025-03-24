c8d93fe0c46ac23306eb1721010ec56a36319f52
Andrey Tabolin <tabolin_andrey@mail.ru>
2025-03-24 21:13:35 +0300

    Добавлен паттерн репозиторий

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -6,6 +6,7 @@ from src.database import async_session_maker
 
 from src.schemas.hotels import HotelPOST, HotelPUT, HotelPATCH
 from src.models.hotels import HotelsOrm
+from src.repositories.hotels import HotelsRepository
 from src.assets.openapi_examples.hotels import (
     POST_OPENAPI_EXAMPLE,
     PUT_OPENAPI_EXAMPLE,
@@ -19,33 +20,22 @@ router = APIRouter(prefix="/hotels", tags=["Отели"])
 async def get_hotels(
     pagination: PaginationDep,
     title: str = Query(default=None, description="Название"),
-    location: int = Query(default=None, description="Адрес"),
+    location: str = Query(default=None, description="Адрес"),
 ):
+
     async with async_session_maker() as session:
-        query = select(HotelsOrm)
-
-        if title:
-            query = query.filter(HotelsOrm.title.ilike(f"%{title}%"))
-        if location:
-            query = query.filter(HotelsOrm.location.ilike(f"%{location}%"))
-
-        query = query.limit(pagination.per_page).offset(
-            pagination.per_page * (pagination.page - 1)
+        return await HotelsRepository(session).get_all(
+            title=title,
+            location=location,
+            limit=pagination.per_page,
+            offset=pagination.per_page * (pagination.page - 1),
         )
-
-        result = await session.execute(query)
-        hotels = result.scalars().all()
-        return hotels
 
 
 @router.get("/hotels/{id}", summary="Получить информацию по отелю")
 async def get_hotel(id: int = Path(description="ИД отеля")):
     async with async_session_maker() as session:
-        query = select(HotelsOrm).filter_by(id=id)
-
-        result = await session.execute(query)
-        hotel = result.scalars().all()
-        return hotel
+        return await HotelsRepository(session).get_one_or_none(id=id)
 
 
 @router.post("/hotels", summary="Создать отель")
@@ -53,10 +43,11 @@ async def create_hotel(
     hotel_data: HotelPOST = Body(openapi_examples=POST_OPENAPI_EXAMPLE),
 ):
     async with async_session_maker() as session:
-        add_hotel_stmt = insert(HotelsOrm).values(**hotel_data.model_dump())
-        await session.execute(add_hotel_stmt)
+        result = await HotelsRepository(session).add(**hotel_data.model_dump())
+        await session.flush()
+        id = result.scalars().one_or_none()
         await session.commit()
-    return hotel_data.model_dump()
+        return await HotelsRepository(session).get_one_or_none(id=id)
 
 
 @router.put("/hotels/{id}", summary="Обновить все поля отеля")
@@ -99,9 +90,7 @@ async def edit_hotel(
 @router.delete("/hotels/{id}", summary="Удалить отель")
 async def delete_hotels(id: int):
     async with async_session_maker() as session:
-        delete_hotel_stmt = (
-            delete(HotelsOrm).filter_by(id=id)
-        )
+        delete_hotel_stmt = delete(HotelsOrm).filter_by(id=id)
         await session.execute(delete_hotel_stmt)
         await session.commit()
     return {"status": "OK"}
---
+++ src/repositories/rooms.py
@@ -0,0 +1,5 @@
+from src.repositories.base import BaseRepository
+from src.models.rooms import RoomsOrm
+
+class RoomsRepository(BaseRepository):
+    model = RoomsOrm
