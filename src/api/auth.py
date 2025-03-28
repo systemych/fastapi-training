fa1326d735d621f67fb2915ae887b553a76ace71
Andrey Tabolin <tabolin_andrey@mail.ru>
2025-03-28 13:15:03 +0300

    Добавил получение токена из запроса

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Body, HTTPException, status, Response
+from fastapi import APIRouter, Body, HTTPException, status, Response, Request
 
 from src.database import async_session_maker
 from src.schemas.users import UserRegister, UserAdd
@@ -6,7 +6,10 @@ from src.repositories.users import UsersRepository
 from src.services.auth import AuthService
 from src.assets.openapi_examples.users import CREATE_USER_EXAMPLE, LOGIN_USER_EXAMPLE
 
+import json
+
 router = APIRouter(prefix="/auth", tags=["Аутентификация и авторизация"])
+
 
 @router.post("/register", summary="Создать пользователя")
 async def register_user(
@@ -33,14 +36,26 @@ async def register_user(
     data: UserRegister = Body(openapi_examples=LOGIN_USER_EXAMPLE),
 ):
     async with async_session_maker() as session:
-        user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
+        user = await UsersRepository(session).get_user_with_hashed_password(
+            email=data.email
+        )
         if not user:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
             )
         if not AuthService().verify_password(data.password, user.hashed_password):
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect password")
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect password"
+            )
 
         access_token = AuthService().create_access_token({"user_id": user.id})
         response.set_cookie("access_token", access_token)
         return {"access_token": access_token}
+
+
+@router.get("/only_auth")
+async def only_auth(request: Request):
+    access_token = (
+        request.cookies["access_token"] if "access_token" in request.cookies else None
+    )
+    return access_token
