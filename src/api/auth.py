6abbad8a130a3e648a78b509192a00813d38574d
Andrey Tabolin <tabolin_andrey@mail.ru>
2025-03-28 12:37:48 +0300

    Добавлен куки и валидация пароля

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Body, HTTPException, status
+from fastapi import APIRouter, Body, HTTPException, status, Response
 from passlib.context import CryptContext
 import jwt
 from datetime import datetime, timezone, timedelta
@@ -15,6 +15,8 @@ SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
@@ -50,13 +52,18 @@ async def register_user(
 
 @router.post("/login", summary="Аутентифицировать пользователя")
 async def register_user(
+    response: Response,
     data: UserRegister = Body(openapi_examples=LOGIN_USER_EXAMPLE),
 ):
     async with async_session_maker() as session:
-        user = await UsersRepository(session).get_one_or_none(email=data.email)
+        user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
         if not user:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
             )
+        if not verify_password(data.password, user.hashed_password):
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect password")
+
         access_token = create_access_token({"user_id": user.id})
+        response.set_cookie("access_token", access_token)
         return {"access_token": access_token}
