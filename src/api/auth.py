8268d33513c15703bb883bdc81a9e601af911029
Andrey Tabolin <tabolin_andrey@mail.ru>
2025-03-28 12:51:49 +0300

    Логика выделена в сервис

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,41 +1,18 @@
 from fastapi import APIRouter, Body, HTTPException, status, Response
-from passlib.context import CryptContext
-import jwt
-from datetime import datetime, timezone, timedelta
 
 from src.database import async_session_maker
 from src.schemas.users import UserRegister, UserAdd
 from src.repositories.users import UsersRepository
+from src.services.auth import AuthService
 from src.assets.openapi_examples.users import CREATE_USER_EXAMPLE, LOGIN_USER_EXAMPLE
 
 router = APIRouter(prefix="/auth", tags=["Аутентификация и авторизация"])
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(
-            minutes=ACCESS_TOKEN_EXPIRE_MINUTES
-        )
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
 
 @router.post("/register", summary="Создать пользователя")
 async def register_user(
     data: UserRegister = Body(openapi_examples=CREATE_USER_EXAMPLE),
 ):
-    hashed_password = pwd_context.hash(data.password)
+    hashed_password = AuthService().hash_password(data.password)
     new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
     async with async_session_maker() as session:
         user_with_same_email = await UsersRepository(session).get_one_or_none(
@@ -61,9 +38,9 @@ async def register_user(
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
             )
-        if not verify_password(data.password, user.hashed_password):
+        if not AuthService().verify_password(data.password, user.hashed_password):
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect password")
 
-        access_token = create_access_token({"user_id": user.id})
+        access_token = AuthService().create_access_token({"user_id": user.id})
         response.set_cookie("access_token", access_token)
         return {"access_token": access_token}
---
+++ src/services/auth.py
@@ -0,0 +1,26 @@
+from passlib.context import CryptContext
+import jwt
+from datetime import datetime, timezone, timedelta
+
+from src.config import settings
+
+class AuthService:
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+    def create_access_token(self, data: dict, expires_delta: timedelta | None = None):
+        to_encode = data.copy()
+        if expires_delta:
+            expire = datetime.now(timezone.utc) + expires_delta
+        else:
+            expire = datetime.now(timezone.utc) + timedelta(
+                minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+            )
+        to_encode.update({"exp": expire})
+        encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
+        return encoded_jwt
+
+    def verify_password(self, plain_password, hashed_password):
+        return self.pwd_context.verify(plain_password, hashed_password)
+
+    def hash_password(self, password: str) -> str:
+        return self.pwd_context.hash(password)
