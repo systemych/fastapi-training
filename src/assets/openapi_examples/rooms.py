c73d860d866adefc794273a59b6d6fd63e773e6a
Andrey Tabolin <tabolin_andrey@mail.ru>
2025-04-13 14:26:24 +0300

    Добавлены rooms

---
+++ src/assets/openapi_examples/rooms.py
@@ -0,0 +1,12 @@
+CREATE_ROOM_EXAMPLE = {
+    "1": {"summary": "Новый объект", "value": {"hotel_id": 1, "title": "3 кровати", "description": "Номер для всей семьи", "price": 5000, "quantity": 10}}
+}
+
+UPDATE_ROOM_EXAMPLE = {
+    "1": {"summary": "Все поля", "value": {"title": "3 кровати", "description": "Номер для всей семьи", "price": 6000, "quantity": 5}}
+}
+
+EDIT_ROOM_EXAMPLE = {
+    "1": {"summary": "Все поля", "value": {"title": "3 кровати", "description": "Номер для всей семьи", "price": 6000, "quantity": 5}},
+    "2": {"summary": "Часть полей", "value": {"price": 6000, "quantity": 5}}
+}
--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -1,5 +1,7 @@
 from src.repositories.base import BaseRepository
 from src.models.rooms import RoomsOrm
+from src.schemas.rooms import RoomSchema
 
 class RoomsRepository(BaseRepository):
     model = RoomsOrm
+    schema = RoomSchema
