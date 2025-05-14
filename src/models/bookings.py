e9a150cbae9ca981d77aaa8ec50a21fdfe6c7704
Andrey Tabolin <tabolin_andrey@mail.ru>
2025-05-14 18:06:03 +0300

    Добавлен total_price

--- src/models/bookings.py
+++ src/models/bookings.py
@@ -1,6 +1,7 @@
 import datetime
 
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy import ForeignKey
 
 from src.database import Base
@@ -11,6 +12,10 @@ class BookingsOrm(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     room_id: Mapped[int] = mapped_column(ForeignKey("rooms.id"))
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    date_from: Mapped[datetime.datetime]
-    date_to: Mapped[datetime.datetime]
+    date_from: Mapped[datetime.date]
+    date_to: Mapped[datetime.date]
     price: Mapped[int]
+
+    @hybrid_property
+    def total_price(self) -> int:
+        return self.price * (self.date_to - self.date_from).days
