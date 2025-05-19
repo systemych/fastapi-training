23510af2a7db8b28e57079610c5c96b0af64c837
Andrey Tabolin <tabolin_andrey@mail.ru>
2025-05-19 18:04:06 +0300

    Добавлена таблица options

---
+++ src/models/options.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, ForeignKey
+
+from src.database import Base
+
+
+class OptionsOrm(Base):
+    __tablename__ = "options"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(127))
+
+class RoomsOptionsOrm(Base):
+    __tablename__ = "rooms_options"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    room_id: Mapped[int] = mapped_column(ForeignKey(column="rooms.id", name="rooms_options_room_id_idx"))
+    option_id: Mapped[int] = mapped_column(ForeignKey(column="options.id", name="rooms_options_option_id_idx"))
