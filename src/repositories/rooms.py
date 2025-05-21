617d20d323b2a611176a04d015f3e6a74fd341ce
Andrey Tabolin <tabolin_andrey@mail.ru>
2025-05-21 19:53:50 +0300

    Добавлены опции для ручки получения номера

--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -14,7 +14,9 @@ class RoomsRepository(BaseRepository):
     async def get_all(self, hotel_id, date_from, date_to):
         # не нравится, как реализовано, в техдолг
         if date_from or date_to:
-            query_get_rooms_by_date = get_rooms_by_date(hotel_id=hotel_id, date_from=date_from, date_to=date_to)
+            query_get_rooms_by_date = get_rooms_by_date(
+                hotel_id=hotel_id, date_from=date_from, date_to=date_to
+            )
 
             rooms_by_date = await self.session.execute(query_get_rooms_by_date)
             result = []
@@ -27,10 +29,7 @@ class RoomsRepository(BaseRepository):
                 for model in result
             ]
 
-        query = (
-            select(self.model)
-            .options(selectinload(self.model.options))
-        )
+        query = select(self.model).options(selectinload(self.model.options))
 
         if hotel_id:
             query = query.filter_by(hotel_id=hotel_id)
@@ -40,3 +39,18 @@ class RoomsRepository(BaseRepository):
             RoomWithOptionsSchema.model_validate(model, from_attributes=True)
             for model in result.scalars().all()
         ]
+
+    async def get_one_or_none(self, **filter_by):
+        query = (
+            select(self.model)
+            .options(selectinload(self.model.options))
+            .filter_by(**filter_by)
+        )
+
+        result = await self.session.execute(query)
+        model = result.scalars().one_or_none()
+
+        if model is None:
+            return None
+
+        return RoomWithOptionsSchema.model_validate(model, from_attributes=True)
