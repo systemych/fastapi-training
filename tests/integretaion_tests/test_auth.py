64d7a0a4cb467778b566154b232bcbc1ff8de5b9
Andrey Tabolin <tabolin_andrey@mail.ru>
2025-05-30 18:03:42 +0300

    Добавлены первые юнит тесты

---
+++ tests/integretaion_tests/test_auth.py
@@ -0,0 +1,9 @@
+from src.services.auth import AuthService
+
+def test_decode_and_encode_access_token():
+    data = {"user_id": 1}
+    jwt_token = AuthService().create_access_token(data)
+
+    payload = AuthService().decode_token(jwt_token)
+    assert payload
+    assert payload["user_id"] == data["user_id"]
