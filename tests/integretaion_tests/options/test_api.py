433a6f8d43492f82bf86e01245485b1ed9754d21
Andrey Tabolin <tabolin_andrey@mail.ru>
2025-06-09 18:31:02 +0300

    Обновил тесты

--- tests/integretaion_tests/options/test_api.py
+++ tests/integretaion_tests/options/test_api.py
@@ -1,6 +1,12 @@
-async def test_api(ac):
+async def test_get_options(ac):
     response = await ac.get("/options", follow_redirects=True)
     assert response.status_code == 200
+    assert isinstance(response.json(), list)
 
-    response = await ac.post("/options", follow_redirects=True, json={"title": "Холодильник"})
+async def test_create_option(ac):
+    option = "Холодильник"
+    response = await ac.post("/options", follow_redirects=True, json={"title": option})
     assert response.status_code == 200
+    res = response.json()
+    assert isinstance(res, dict)
+    assert res["title"] == option
