c6f7e51e6826442c0b3dfa78451ccc79d1b14475
Andrey Tabolin <tabolin_andrey@mail.ru>
2025-06-09 22:18:18 +0300

    Добавил АПИ тест на добавление бронирований

--- tests/integretaion_tests/bookings/test_api.py
+++ tests/integretaion_tests/bookings/test_api.py
@@ -3,13 +3,22 @@ async def test_get_bookings(ac):
     assert response.status_code == 200
 
 
-async def test_add_booking(db, ac):
-    room_id = room_id = (await db.rooms.get_all())[0].id
+async def test_add_bookings(db, ac):
+    room = (await db.rooms.get_all())[0]
+
+    for i in range(0, room.quantity):
+        response = await ac.post(
+            "/bookings",
+            follow_redirects=True,
+            json={"room_id": room.id, "date_from": "2025-06-09", "date_to": "2025-06-10"},
+        )
+        assert response.status_code == 200
+        res = response.json()
+        assert isinstance(res, dict)
+
     response = await ac.post(
         "/bookings",
         follow_redirects=True,
-        json={"room_id": room_id, "date_from": "2025-06-09", "date_to": "2025-06-09"},
+        json={"room_id": room.id, "date_from": "2025-06-09", "date_to": "2025-06-10"},
     )
-    assert response.status_code == 200
-    res = response.json()
-    assert isinstance(res, dict)
+    assert response.status_code == 400
