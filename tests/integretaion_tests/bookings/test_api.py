b1b6761f3ccc0b12444b72558ed5e16dbda5e382
Andrey Tabolin <tabolin_andrey@mail.ru>
2025-06-10 21:26:38 +0300

    Добавил тест на создание и получение своих бронирований

--- tests/integretaion_tests/bookings/test_api.py
+++ tests/integretaion_tests/bookings/test_api.py
@@ -14,7 +14,7 @@ async def test_get_bookings(ac):
         (1, "2025-06-09", "2025-06-10", 200),
         (1, "2025-06-09", "2025-06-10", 200),
         (1, "2025-06-09", "2025-06-10", 200),
-        (1, "2025-06-09", "2025-06-10", 200),
+        (1, "2025-06-09", "2025-06-10", 400),
     ],
 )
 async def test_add_bookings(room_id, date_from, date_to, status_code, db, ac):
@@ -27,3 +27,31 @@ async def test_add_bookings(room_id, date_from, date_to, status_code, db, ac):
     if status_code == 200:
         res = response.json()
         assert isinstance(res, dict)
+
+
+@pytest.fixture(scope="session")
+async def delete_all_bookings(db):
+    await db.bookings.delete()
+    await db.commit()
+
+
+@pytest.mark.parametrize(
+    "room_id, date_from, date_to, counter",
+    [
+        (1, "2025-06-09", "2025-06-10", 1),
+        (1, "2025-06-09", "2025-06-10", 2),
+        (1, "2025-06-09", "2025-06-10", 3),
+    ],
+)
+async def test_add_and_get_my_bookings(room_id, date_from, date_to, counter, delete_all_bookings, db, ac):
+    await ac.post(
+        "/bookings",
+        follow_redirects=True,
+        json={"room_id": room_id, "date_from": date_from, "date_to": date_to},
+    )
+    response = await ac.get(
+        "/bookings/me",
+        follow_redirects=True
+    )
+
+    assert len(response.json()) == counter
